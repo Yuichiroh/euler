16f918ef5167a44ac960ae46610968ef340bc99b
Yuichiroh Matsubayashi <y-matsu@ecei.tohoku.ac.jp>
2015-01-01 08:43:02 +0900

    book

--- src/main/scala/yuima/euler/YearOutInPrimes.scala
+++ src/main/scala/yuima/euler/YearOutInPrimes.scala
@@ -6,12 +6,13 @@ package yuima.euler
  *         Created on 15/01/01.
  */
 object PrimeOutInYears extends App {
-  val primeOutInYears = (1 to 3000).filter(year => isPrimes(yearOutAndIn(year)))
+  val bound = args(0).toLong
+
+  val primeOutInYears = (1L to bound).filter(year => isPrimes(yearOutAndIn(year)))
 
   def yearOutAndIn(year: Long) = Seq(year * 10000 + 1231, (year + 1) * 10000 + 101)
 
   def isPrimes(ns: Seq[Long]) = ns.forall(isPrimeMillerRabin)
 
-  println((2000 to 2010).map(yearOutAndIn(_)))
   println(primeOutInYears)
 }
