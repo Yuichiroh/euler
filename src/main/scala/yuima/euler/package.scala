957811ed163374e45f05d26fc5221e84fa6f2983
Yuichiroh Matsubayashi <y-matsu@ecei.tohoku.ac.jp>
2015-01-01 10:32:12 +0900

    book

--- src/main/scala/yuima/euler/package.scala
+++ src/main/scala/yuima/euler/package.scala
@@ -1,27 +1,6 @@
 package yuima
 
 package object euler {
-  class Fraction private (val numerator: BigInt, val denominator: BigInt) {
-    override def toString = {
-      if (denominator != 1) new StringBuilder(numerator.toString).append("/").append(denominator.toString).toString
-      else numerator.toString
-    }
-
-    def *(f: Fraction) = Fraction(numerator * f.numerator, denominator * f.denominator)
-    def +(f: Fraction) = Fraction(numerator * f.denominator + denominator * f.numerator, denominator * f.denominator)
-    def ==(f: Fraction) = { if (numerator == f.numerator && denominator == f.denominator) true else false }
-    implicit def FractionInt(n: BigInt): Fraction = new Fraction(n, 1)
-  }
-
-  object Fraction {
-    def apply(num: BigInt, den: BigInt) = {
-      val d = gcd(num, den)
-      new Fraction(num / d, den / d)
-    }
-
-    def gcd(m: BigInt, n: BigInt): BigInt = if (n == 0) m else gcd(n, m % n)
-  }
-
   def gcd(m: Int, n: Int): Int = if (n == 0) m else gcd(n, m % n)
 
   def lcm(m: Int, n: Int): Int = m * (n / gcd(m, n))
@@ -39,10 +18,11 @@ package object euler {
     case 0 => Nil
     case _ => (n % 10) :: digits(n / 10)
   }
-  
+
   def count(list: List[Long]) = (Map[Long, Int]().withDefaultValue(0) /: list) { (m, k) => m + (k -> (m(k) + 1)) }
 
   def triangleNumber(num: Int): Int = if (num == 0) 0 else num + triangleNumber(num - 1)
+
   def triangleNumbers = Iterator.from(1).map(triangleNumber)
 
   def isPrime(n: Int) = Iterator.from(2).takeWhile(m => m * m <= n).forall(n % _ != 0)
@@ -67,6 +47,13 @@ package object euler {
   }
 
   def isPrimeMillerRabin(n: Long) = {
+    def divBy2(n: Long): Long = if (n % 2 != 0) n else divBy2(n / 2)
+
+    def pow(base: Long, power: Long, mod: Long, result: Long): Long = {
+      if (power > 0) pow((base * base) % mod, power >> 1, mod, if ((power & 1) == 1) (result * base) % mod else result)
+      else result
+    }
+
     if (n == 2) true
     else if (n % 2 == 0) false
     else if (n < 9) true
@@ -74,23 +61,16 @@ package object euler {
     else {
       val d = divBy2(n - 1)
       val pseudoPrimes = Set(25326001L, 161304001L, 960946321L, 1157839381L, 3215031751L, 3697278427L, 5764643587L, 6770862367L, 14386156093L, 15579919981L, 18459366157L, 19887974881L, 21276028621L)
-      !pseudoPrimes.contains(n) && List(2, 3, 5).forall(a => {
+      !pseudoPrimes.contains(n) && Seq(2, 3, 5).forall { a =>
         var t = d
         var y = pow(a, t, n, 1L)
-        while ((t != n - 1) && (y != 1) && (y != n - 1)) {
+        while (t != n - 1 && y != 1 && y != n - 1) {
           y = (y * y) % n
           t <<= 1
         }
         y == n - 1 || (t & 1) != 0
-      })
+      }
     }
-  }
-
-  def divBy2(n: Long): Long = if (n % 2 != 0) n else divBy2(n / 2)
-
-  def pow(base: Long, power: Long, mod: Long, result: Long): Long = {
-    if (power > 0) pow((base * base) % mod, power >> 1, mod, if ((power & 1) == 1) (result * base) % mod else result)
-    else result
   }
 
   //  def fib(a: Int = 1, b: Int = 1): Stream[Int] = a #:: fib(b, a + b)
@@ -98,7 +78,33 @@ package object euler {
 
   def sumOfDivisers(num: Int) = factorize(num).groupBy(prime => prime).map(e => (0 to e._2.length).map(k => BigInt(e._1).pow(k)).sum.toInt).product - num
 
+  class Fraction private(val numerator: BigInt, val denominator: BigInt) {
+    override def toString = {
+      if (denominator != 1) new StringBuilder(numerator.toString).append("/").append(denominator.toString).toString
+      else numerator.toString
+    }
+
+    def *(f: Fraction) = Fraction(numerator * f.numerator, denominator * f.denominator)
+
+    def +(f: Fraction) = Fraction(numerator * f.denominator + denominator * f.numerator, denominator * f.denominator)
+
+    def ==(f: Fraction) = {
+      if (numerator == f.numerator && denominator == f.denominator) true else false
+    }
+
+    implicit def FractionInt(n: BigInt): Fraction = new Fraction(n, 1)
+  }
+
   implicit class RichInt(val n: Int) {
     def ! : BigInt = (BigInt(1) to n).product
+  }
+
+  object Fraction {
+    def apply(num: BigInt, den: BigInt) = {
+      val d = gcd(num, den)
+      new Fraction(num / d, den / d)
+    }
+
+    def gcd(m: BigInt, n: BigInt): BigInt = if (n == 0) m else gcd(n, m % n)
   }
 }
